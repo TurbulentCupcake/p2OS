ed3ebbff42dd7c5c5e510e1ecbe518544551ee69
Adithya Murali <adithya@royal-16.cs.wisc.edu>
2017-09-29 15:53:40 -0500

    added ls command functionality

--- mysh.c
+++ mysh.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <sys/types.h>
 
 #define BUFFERSIZE 128
 char buffer[BUFFERSIZE];
@@ -70,7 +72,9 @@ char ** parseCommand(char * command) {
 // function that prints the current working directory
 void pwd() { 	
 	
-	getcwd(CWDBUFFER, sizeof(char)*1000);
+	if(getcwd(CWDBUFFER, sizeof(char)*1000) == NULL) { 
+		// handle error if unable to print current wd
+	}	
 	printf("%s\n", CWDBUFFER);
 }
 
@@ -79,14 +83,51 @@ void pwd() {
 // specifided by the user 
 void wd(char ** commandArgs){ 
 
+
 	if(commandArgs[1] == NULL) { 
-		chdir(getenv("HOME"));
+		if(chdir(getenv("HOME")) < 0) { 
+			// handle error if unable to go to home
+		}
 	} else {
+		if(chdir(commandArgs[1]) < 0)  {
+			// handle error if unable to reach specified directoryh
+		}
 	}
-		
 		
 	
 }
+
+// function that lists all the files in a directory
+void ls(char ** commandArgs) { 
+
+	int status;
+	// fork a child
+	pid_t rc = fork();
+	if(rc == 0) {
+		char * argv[4];
+		argv[0] = strdup("/bin/ls");
+		if(commandArgs[1] != NULL) {
+			argv[1] = strdup(commandArgs[1]);
+			argv[2] = NULL;
+			if(execvp(argv[0], argv) < 0) { 
+				// error check if the ls command didnt execute
+			}
+		} else if (commandArgs[1] == NULL) {
+			argv[1] = NULL;
+			if (execvp(argv[0], argv) < 0) { 
+				// error check if the ls command didnt execute
+			}
+		}
+	
+	} else if (rc > 0) {			
+		 (void) waitpid(rc, &status, WUNTRACED | WCONTINUED);
+	} else { 
+		// throw an error
+	}
+		
+	
+}
+
 
 
 // test function to print arguments
@@ -122,8 +163,8 @@ for(;;) {
 		pwd(); // if the user calls pwd, then pwd
 	} else if (strcmp(commandArgs[0], "cd") == 0) {
 		wd(commandArgs);		
-	} else {
-	//	printf("command not regonized\n");
+	} else if (strcmp(commandArgs[0], "ls") == 0) {
+		ls(commandArgs);
 	}
 	
 	free(command);
